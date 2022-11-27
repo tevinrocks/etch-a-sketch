81255e6229e6d0c1da464084be739ff6aba91cde
Derrick Teartt <d_teartt@aol.com>
2022-11-27 07:30:48 -0800

    etch-a-sketch now has rainbow format

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const grid = document.querySelector('#grid');
+const grid = document.querySelector('#grid'); //var of container div
 
 for (let i = 0; i < 256; i++){
   const cell = document.createElement("div");
+  let r = Math.floor(Math.random() * 256) ; //random num 1-256
+  let g = Math.floor(Math.random() * 256) ; //random num 1-256
+  let b = Math.floor(Math.random() * 256) ; //random num 1-256
   cell.classList.add("cell");
   cell.innerText = `Cell ${i}`;
 
   cell.addEventListener('mouseover', e => {
-    e.target.classList.add('change');
+    e.target.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    // e.target.classList.add('change');
   })
 
   grid.appendChild(cell);
